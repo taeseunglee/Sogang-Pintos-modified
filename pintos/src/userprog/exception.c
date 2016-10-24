ba8c3e750dfd9c48b78443d4ab0264269f9eb654
Taeseung Lee <dlxotmd125@gmail.com>
2016-10-24 23:39:31 +0900

    modfify exception - latter We should modify thread_exit() -> syscall_exit()

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -151,11 +151,16 @@ page_fault (struct intr_frame *f)
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
+  thread_exit();
+  // TODO chagne to!!!
+  // syscall_exit();
+/*  
   printf ("Page fault at %p: %s error %s page in %s context.\n",
           fault_addr,
           not_present ? "not present" : "rights violation",
           write ? "writing" : "reading",
           user ? "user" : "kernel");
   kill (f);
+*/
 }
 
