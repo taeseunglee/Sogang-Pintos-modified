c83584666f91e5f8757c1e3e80cac6d22b4f4de5
dward1001 <dward1001a@gmail.com>
2016-10-26 17:39:29 +0900

    lib/user/syscall.c - modify assembly code

--- pintos/src/lib/user/syscall.c
+++ pintos/src/lib/user/syscall.c
@@ -61,6 +61,23 @@
           retval;                                               \
         })
 
+/* Invokes syscall NUMBER, passing arguments ARG0, ARG1, ARG2
+   and ARG3, and returns the return value as an `int'. */
+#define syscall4(NUMBER, ARG0, ARG1, ARG2, ARG3)                                \
+        ({                                                                      \
+          int retval;                                                           \
+          asm volatile                                                          \
+            ("pushl %[arg3]; pushl %[arg2]; pushl %[arg1]; pushl %[arg0]; "     \
+             "pushl %[number]; int $0x30; addl $20, %%esp"                      \
+               : "=a" (retval)                                                  \
+               : [number] "i" (NUMBER),                                         \
+                 [arg0] "g" (ARG0),                                             \
+                 [arg1] "g" (ARG1),                                             \
+                 [arg2] "g" (ARG2),                                             \
+                 [arg3] "g" (ARG3)                                              \
+               : "memory");                                                     \
+          retval;                                                               \
+        })
 void
 halt (void) 
 {
